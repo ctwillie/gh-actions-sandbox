4f91645dae87d1d0f7c101104d16bc6c2e9aa27f
Cedric Twillie <32856661+ctwillie@users.noreply.github.com>
2023-11-06 22:52:43 -0500

    ci: add cicd workflow files (#1)
    * ci: change workflow file names
    
    * ci: change wf branch to development
    
    * ci: change readme
    
    * ci: create project checks wf
    
    * feat: add a duplicate route name
    
    * feat: only run when routes change
    
    * chore: edit readme
    
    * chore: clean up files
    
    * ci: run tests on master pr
    
    ---------
    
    Co-authored-by: Cedric Twillie <cedric.twillie@1nhealth.com>

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+// add a duplicate route name
+Route::get('/foo', function () {
+    return 'foo';
+})->name('foo');
