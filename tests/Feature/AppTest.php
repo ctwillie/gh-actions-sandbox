e3bd208cdf614947d029bf909889a9161f836015
Cedric Twillie <cedric.twillie@1nhealth.com>
2023-11-11 20:54:06 -0500

    test: use passing test

--- tests/Feature/AppTest.php
+++ tests/Feature/AppTest.php
@@ -8,6 +8,6 @@ class AppTest extends TestCase
 {
     public function test_the_application_returns_a_successful_response(): void
     {
-        $this->get('/')->assertStatus(400);
+        $this->get('/')->assertStatus(200);
     }
 }
